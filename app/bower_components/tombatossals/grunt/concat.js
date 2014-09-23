0092749ca21e94434cb530228473a7e2d82bcd4f
awahab07 <awahab07@yahoo.com>
2014-09-23 12:50:08 +0500

    Tracked bower_components and node_modules. Also a full build of openlayers3 is present in bower_components/openlayers3/build

---
+++ app/bower_components/tombatossals/grunt/concat.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function (grunt, options) {
+    return {
+        dist: {
+            options: {
+                banner: '(function() {\n\n"use strict";\n\n',
+                footer: '\n}());'
+            },
+            src: [
+                'src/directives/openlayers.js',
+                'src/directives/center.js',
+                'src/directives/layers.js',
+                'src/services/*.js'
+            ],
+            dest: 'dist/angular-openlayers-directive.pre.js',
+        },
+        license: {
+            src: [
+                'src/header-MIT-license.txt',
+                'dist/angular-openlayers-directive.min.no-header.js'
+            ],
+            dest: 'dist/angular-openlayers-directive.min.js',
+        },
+    };
+};
